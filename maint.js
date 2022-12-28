d20bca464c8fa90756850c86b1d8839e89c1a09e
Chris V <Countorlak@users.noreply.github.com>
2022-12-28 03:48:42 -0500

    Update and rename maint_Ver1_0_0.js to maint.js
    Fixed endless loop on line 16 where all the variables were declared with const and some needed let.

--- maint.js
+++ maint.js
@@ -1,22 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
 
+// Version 1.0.1
 // Use this file like: ./maint.js server-name -t #
 // Will take 1 arg for targeted server then maintain hacks, weakenings, and grow at optimal levels.
 
 	while(true){
-		const srv = ns.args[0];
-		const srvSec = ns.getServerSecurityLevel(srv);
+		let srv = ns.args[0];
+		let srvSec = ns.getServerSecurityLevel(srv);
 		const srvSecMin = ns.getServerMinSecurityLevel(srv);
-		const srvFunds = ns.getServerMoneyAvailable(srv);
+		let srvFunds = ns.getServerMoneyAvailable(srv);
 		const srvFundsMax = ns.getServerMaxMoney(srv);
 
 		while(srvSec > srvSecMin + 1){
-			await ns.weaken(srv)
+			await ns.weaken(srv);
 		}
 
 		while(srvFunds < srvFundsMax * 0.9){
-			await ns.grow(srv)
+			await ns.grow(srv);
 		}
 
 		await ns.hack(srv);
